6361f8e2f06096bb20696075993b15669b0b5d28
Vinh <118822581+dangvinh5304@users.noreply.github.com>
2025-01-02 14:55:44 +0700

    Add files via upload

---
+++ function_documents.py
@@ -0,0 +1,18 @@
+def firstDegree(a, b):
+    '''
+    Đây là phương trình bậc 1: ax+b=0
+    :param a: hệ số a
+    :param b: hệ số b
+    :return: trả về 3 trường hợp kết quả
+    '''
+    if a==0 and b==0:
+        print ("Phương trình vô số nghiệm")
+    elif a==0 and b!=0:
+        print ("Phương trình vô nghiệm")
+    else:
+        x=-b/a
+        print ("No của phương trình =", x)
+print("Phương trình bậc 1")
+a=float(input("Nhập hệ số a: "))
+b=float(input("Nhập hệ số b: "))
+firstDegree(a, b)
