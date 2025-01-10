90cf6d1c8bf5e589a127d02e0d1c86fe21f7087d
Vinh <118822581+dangvinh5304@users.noreply.github.com>
2025-01-10 09:39:29 +0700

    Delete testreadproduct.py

--- testreadproduct.py
+++
@@ -1,7 +0,0 @@
-from ontap_oop.filefactory import FileFactory
-from ontap_oop.product import Product
-ff = FileFactory()
-dataset = ff.readData("mydataset.json", Product)
-print("Danh sách sản phẩm: ")
-for product in dataset:
-    print(product)
