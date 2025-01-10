dfabee3aad4e0afa31a1db1344fdbaa3af007ee0
Vinh <118822581+dangvinh5304@users.noreply.github.com>
2025-01-10 09:40:47 +0700

    Delete TestHelloWorldDialogUI.py

--- TestHelloWorldDialogUI.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QDialog
-from HelloWorldDialog import Ui_Dialog
-
-app=QApplication([])
-dialog=QDialog()
-window = Ui_Dialog()
-window.setupUi(dialog)
-dialog.show()
-app.exec()
