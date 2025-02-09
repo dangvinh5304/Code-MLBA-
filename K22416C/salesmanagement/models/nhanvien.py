f86ac355ad937f4568f1043ca3e169a83e35b5da
Vinh <118822581+dangvinh5304@users.noreply.github.com>
2025-02-09 17:43:59 +0700

    Add files via upload

---
+++ K22416C/salesmanagement/models/nhanvien.py
@@ -0,0 +1,8 @@
+class NhanVien:
+    def __init__(self, id = None, manhanvien = None, tennhanvien = None, username = None, password = None, isdelete = None):
+        self.id = id
+        self.manhanvien = manhanvien
+        self.tennhanvien = tennhanvien
+        self.username = username
+        self.password = password
+        self.isdelete = isdelete
